528fb3b590a5565a9a940944f7f100b7494b5141
Lambert D <lambertdem@hotmail.com>
2019-09-19 21:39:05 -0400

    add code k_fold

--- code/k_fold.py
+++ code/k_fold.py
@@ -23,13 +23,12 @@ def k_fold(dataframe, k): #takes in a dataframes, returns list of k dataframes
         df = dataframe.iloc[ next_row : next_row + quotient]
         folds.append(df)
 
+        next_row += quotient
+
         if i < rem:
             quotient -=1
 
-        next_row += quotient + 1
-
-    print(folds)
-
+    #print(folds)
 
     return folds
 
@@ -40,7 +39,7 @@ def main():
     source = sys.argv[1]
     df = pd.read_csv(source, delimiter = ';')
 
-    k_fold(df,4)
+    k_fold(df, int(sys.argv[2]))
 
 if __name__ == "__main__":
     main()
