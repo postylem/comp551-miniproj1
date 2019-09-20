ef38632b5fb2150a6d285096e746aa3b706b667e
Lambert D <lambertdem@hotmail.com>
2019-09-19 21:28:57 -0400

    add code k_fold

---
+++ code/k_fold.py
@@ -0,0 +1,48 @@
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use('seaborn-whitegrid')
+import pandas as pd
+import sys
+
+def k_fold(dataframe, k): #takes in a dataframes, returns list of k dataframes
+    
+    lines = dataframe.shape[0]
+    quotient = int(lines / k)
+    rem = lines % k
+
+    folds = []
+    next_row = 0
+
+    print(quotient)
+
+    for i in range(k):
+
+        if i < rem:
+            quotient +=1
+        
+        df = dataframe.iloc[ next_row : next_row + quotient]
+        folds.append(df)
+
+        if i < rem:
+            quotient -=1
+
+        next_row += quotient + 1
+
+    print(folds)
+
+
+    return folds
+
+        
+
+def main():
+
+    source = sys.argv[1]
+    df = pd.read_csv(source, delimiter = ';')
+
+    k_fold(df,4)
+
+if __name__ == "__main__":
+    main()
+
+
