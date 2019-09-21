a47981de6eda1bc6f86db5da2aa5f9e918e52f2d
Jacob Louis Hoover <postylem@gmail.com>
2019-09-21 19:18:01 -0400

    Log odds now works.

--- code/LDA.py
+++ code/LDA.py
@@ -1,25 +1,37 @@
 import numpy as np
 
 
-def get_log_odds(data):
-    # gets the predicted log odds : log[ P(y=1|x)/P(y=0|x) ]
+def predict_log_odds(train_data, test_datapoint):
+    # takes in training data in an array, one row per observation,
+    # with true labels being the last row of the data
+    # gets the predicted log odds of test_datapoint being label = 1 vs 0
+    # that is, log[ P(y=1|x)/P(y=0|x) ] , given the assumpions of LDA
 
-    # observations - a numpy array of observations
-    # true_labels - a list of the true labels for each row of observations
+    # make new array, just containing the rows of a given label, while removing that last row
+    observations_labeled_0 = train_data[train_data[:,-1]==0][:,:-1]
+    observations_labeled_1 = train_data[train_data[:,-1]==1][:,:-1]
 
-    observations_labeled_0 = data[data[:,-1]==0][:,:-1]
-    observations_labeled_1 = data[data[:,-1]==1][:,:-1]
+    # some useful constants:
     N_0 = np.size(observations_labeled_0,axis=0)
     N_1 = np.size(observations_labeled_1,axis=0)
+    if (N_0 + N_1 != np.size(train_data,axis=0)):
+        print("Warning! At least one train_data point is not labeled 0 or 1")
     P_0 = N_0 / (N_0 + N_1)
     P_1 = N_1 / (N_0 + N_1)
+
+    # the mean vectors (mean over all train_data points of each label)
     mean_0 = np.mean(observations_labeled_0,0)
     mean_1 = np.mean(observations_labeled_1,0)
-    covariance_matrix = np.cov(np.transpose(data[:,:-1]))
-    invcov = np.linalg.inv(covariance_matrix)
 
-    # covariance matrix calculated on the train data without the last column (the labels)
-    covariance_matrix = np.cov(data[:,:-1])
+    # make the covariance matrix (note we want the covariance matrix over the _rows_ of train_data, hence the transpose)
+    cov_matrix = np.cov(np.transpose(train_data[:,:-1]))
+    inv_cov = np.linalg.inv(cov_matrix)
 
-    log_odds = 0
+    # now to compute the predicted log odds
+    log_odds = (
+        np.log(P_1/P_0)
+        - 0.5 * np.dot(mean_1, np.dot(inv_cov, mean_1))
+        + 0.5 * np.dot(mean_0, np.dot(inv_cov, mean_0))
+        + np.dot(test_datapoint , np.dot(inv_cov, (mean_0 - mean_1))))
+
     return log_odds
